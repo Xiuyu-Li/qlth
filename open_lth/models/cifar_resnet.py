686433900c4a6cb548fa824ad52d9c5e2db0c9ff
Xiuyu Li <xl289@cornell.edu>
2020-12-16 17:32:03 -0500

    Update resnet architecture for lth

--- open_lth/models/cifar_resnet.py
+++ open_lth/models/cifar_resnet.py
@@ -3,6 +3,9 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import math
+
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -12,69 +15,135 @@ from models import base
 from pruning import sparse_global
 
 
+NUM_CLASSES = 10
+
+
+class EltwiseAdd(nn.Module):
+    def __init__(self, inplace=False):
+        """Element-wise addition"""
+        super().__init__()
+        self.inplace = inplace
+
+    def forward(self, *input):
+        res = input[0]
+        if self.inplace:
+            for t in input[1:]:
+                res += t
+        else:
+            for t in input[1:]:
+                res = res + t
+        return res
+
+
+def conv3x3(in_planes, out_planes, stride=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, block_gates, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.block_gates = block_gates
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu1 = nn.ReLU(inplace=False)  # To enable layer removal inplace must be False
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.relu2 = nn.ReLU(inplace=False)
+        self.downsample = downsample
+        self.stride = stride
+        self.residual_eltwiseadd = EltwiseAdd()
+
+    def forward(self, x):
+        residual = out = x
+
+        if self.block_gates[0]:
+            out = self.conv1(x)
+            out = self.bn1(out)
+            out = self.relu1(out)
+
+        if self.block_gates[1]:
+            out = self.conv2(out)
+            out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out = self.residual_eltwiseadd(residual, out)
+        out = self.relu2(out)
+
+        return out
+
+
 class Model(base.Model):
     """A residual neural network as originally designed for CIFAR-10."""
 
-    class Block(nn.Module):
-        """A ResNet block."""
 
-        def __init__(self, f_in: int, f_out: int, downsample=False):
-            super(Model.Block, self).__init__()
+    def __init__(self, block, layers, initializer, num_classes=NUM_CLASSES):
+        self.nlayers = 0
+        # Each layer manages its own gates
+        self.layer_gates = []
+        for layer in range(3):
+            # For each of the 3 layers, create block gates: each block has two layers
+            self.layer_gates.append([])  # [True, True] * layers[layer])
+            for blk in range(layers[layer]):
+                self.layer_gates[layer].append([True, True])
 
-            stride = 2 if downsample else 1
-            self.conv1 = nn.Conv2d(f_in, f_out, kernel_size=3, stride=stride, padding=1, bias=False)
-            self.bn1 = nn.BatchNorm2d(f_out)
-            self.conv2 = nn.Conv2d(f_out, f_out, kernel_size=3, stride=1, padding=1, bias=False)
-            self.bn2 = nn.BatchNorm2d(f_out)
-
-            # No parameters for shortcut connections.
-            if downsample or f_in != f_out:
-                self.shortcut = nn.Sequential(
-                    nn.Conv2d(f_in, f_out, kernel_size=1, stride=2, bias=False),
-                    nn.BatchNorm2d(f_out)
-                )
-            else:
-                self.shortcut = nn.Sequential()
-
-        def forward(self, x):
-            out = F.relu(self.bn1(self.conv1(x)))
-            out = self.bn2(self.conv2(out))
-            out += self.shortcut(x)
-            return F.relu(out)
-
-    def __init__(self, plan, initializer, outputs=None):
+        self.inplanes = 16  # 64
         super(Model, self).__init__()
-        outputs = outputs or 10
+        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self._make_layer(self.layer_gates[0], block, 16, layers[0])
+        self.layer2 = self._make_layer(self.layer_gates[1], block, 32, layers[1], stride=2)
+        self.layer3 = self._make_layer(self.layer_gates[2], block, 64, layers[2], stride=2)
+        self.avgpool = nn.AvgPool2d(8, stride=1)
+        self.fc = nn.Linear(64 * block.expansion, num_classes)
 
-        # Initial convolution.
-        current_filters = plan[0][0]
-        self.conv = nn.Conv2d(3, current_filters, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn = nn.BatchNorm2d(current_filters)
-
-        # The subsequent blocks of the ResNet.
-        blocks = []
-        for segment_index, (filters, num_blocks) in enumerate(plan):
-            for block_index in range(num_blocks):
-                downsample = segment_index > 0 and block_index == 0
-                blocks.append(Model.Block(current_filters, filters, downsample))
-                current_filters = filters
-
-        self.blocks = nn.Sequential(*blocks)
-
-        # Final fc layer. Size = number of filters in last segment.
-        self.fc = nn.Linear(plan[-1][0], outputs)
-        self.criterion = nn.CrossEntropyLoss()
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
 
         # Initialize.
         self.apply(initializer)
 
+    def _make_layer(self, layer_gates, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(layer_gates[0], self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(layer_gates[i], self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
     def forward(self, x):
-        out = F.relu(self.bn(self.conv(x)))
-        out = self.blocks(out)
-        out = F.avg_pool2d(out, out.size()[3])
-        out = out.view(out.size(0), -1)
-        out = self.fc(out)
-        return out
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
 
     @property
     def output_layer_names(self):
@@ -111,19 +180,14 @@ class Model(base.Model):
         if not Model.is_valid_model_name(model_name):
             raise ValueError('Invalid model name: {}'.format(model_name))
 
-        name = model_name.split('_')
-        W = 16 if len(name) == 3 else int(name[3])
-        D = int(name[2])
-        if (D - 2) % 3 != 0:
-            raise ValueError('Invalid ResNet depth: {}'.format(D))
-        D = (D - 2) // 6
-        plan = [(W, D), (2*W, D), (4*W, D)]
-
-        return Model(plan, initializer, outputs)
+        if model_name == 'cifar_resnet_20':
+            layers = [3, 3, 3]
+        
+        return Model(BasicBlock, layers, initializer)
 
     @property
     def loss_criterion(self):
-        return self.criterion
+        return nn.CrossEntropyLoss()
 
     @staticmethod
     def default_hparams():
